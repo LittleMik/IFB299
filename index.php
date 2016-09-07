d74ccdf69a4fe5523fa46c7a0546c84bf3119635
Elstabbo <grgmills8@gmail.com>
2016-09-07 17:50:12 +1000

    Fixed login, and restructured website to make better use of include files

--- index.php
+++ index.php
@@ -1,72 +1,8 @@
-<!DOCTYPE html>
-<html class="skrollr skrollr-desktop">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <link rel="stylesheet" href="css/style.css">
-    <!--<link href="bootstrap.min.css" rel="stylesheet">-->
-
-    <title>On the Spot Package Delivery | Login</title>
-    <!-- Sign in-->
-    <link href="bootstrap-3.3.7/docs/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="bootstrap-3.3.7/docs/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-    <link href="bootstrap-3.3.7/docs/examples/signin/signin.css" rel="stylesheet">
-    <script src="bootstrap-3.3.7/docs/assets/js/ie-emulation-modes-warning.js"></script>
-    <!-- Sign in-->
-    
-    
-	<!--Nav-->
-   <link href="bootstrap-3.3.7/docs/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="bootstrap-3.3.7/docs/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="bootstrap-3.3.7/docs/examples/navbar/navbar.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="bootstrap-3.3.7/docs/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="bootstrap-3.3.7/docs/assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    
-        
-	<!--Nav-->
-</head>
-	
-
-      <!-- Main component for a primary marketing message or call to action -->
-<!--      <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-        </p>
-      </div>
--->
-      
-
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="bootstrap-3.3.7/docs/assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="bootstrap-3.3.7/docs/dist/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="bootstrap-3.3.7/docs/assets/js/ie10-viewport-bug-workaround.js"></script>
+<?php require 'includes/head.inc' ?>
   
 <body>
 	<!--the header, see the file for the code-->
-	<?php include 'header.inc' ?>
+	<?php include 'includes/header.inc' ?>
 	
 	<div class="container">
 	<section>
