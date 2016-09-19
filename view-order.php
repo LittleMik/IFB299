7063c5e395ef07552ba7526c3cf71ecd64faa3a4
Michael Smallcombe <Michael Smallcombe>
2016-09-19 16:11:08 +1000

    More Order Details and Echo fixes (form needs css)

--- view-order.php
+++ view-order.php
@@ -19,113 +19,127 @@
 ?>
 
 <?php
-if($_SERVER["REQUEST_METHOD"] === "GET")
-{
-  //Get Dependancies
-  require_once 'php/formValidation.php';
-
-
-}
-if($_SERVER["REQUEST_METHOD"] === "POST")
-{
-
-
-  $errors = array();
-  $formValid = true;
-
-  //Get Dependancies
-  require_once 'php/formValidation.php';
-
-  //PHP Field Validation
-  $errors = array(
-
-  );
-
-  //Check for presence of errors and output
-  foreach($errors as $field => $valid)
+  if($_SERVER["REQUEST_METHOD"] === "GET")
   {
-	if($valid === false)
-	{
-	  $formValid = false;
-	  echo $_POST[$field] . "<br />";
-	  echo "Invalid " . $field . " detected<br />";
-	}
+    //Get Dependancies
+    require_once 'php/formValidation.php';
+
+
   }
-
-  /*
-  I can see a chronologically ordered list of deliveries that are assigned to me.
-I can click to find specific information on the delivery, including where I am going to, what time I am expected, and any notes the customer added in the order.
-I can click to find specific information on the customer
-*/
-  //Complete Registration Process
-  if($formValid)
+  if($_SERVER["REQUEST_METHOD"] === "POST")
   {
-  	if(isset($_POST['email']))
-  	{
-      require 'php/pdo.inc';
 
-      try{
-    	  $query = "SELECT orders.*, users.firstName, users.lastName
-        FROM orders
-        LEFT JOIN users
-        ON orders.userID=users.userID
-        WHERE users.email LIKE :email";
+
+    $errors = array();
+    $formValid = true;
+
+    //Get Dependancies
+    require_once 'php/formValidation.php';
+
+    //PHP Field Validation
+    $errors = array(
+
+    );
+
+    //Check for presence of errors and output
+    foreach($errors as $field => $valid)
+    {
+  	if($valid === false)
+  	{
+  	  $formValid = false;
+  	  echo $_POST[$field] . "<br />";
+  	  echo "Invalid " . $field . " detected<br />";
+  	}
+    }
+
+    /*
+    I can see a chronologically ordered list of deliveries that are assigned to me.
+  I can click to find specific information on the delivery, including where I am going to, what time I am expected, and any notes the customer added in the order.
+  I can click to find specific information on the customer
+  */
+    //Complete Registration Process
+    if($formValid)
+    {
+      if(isset($_POST['email']))
+    	{
+        require 'php/pdo.inc';
+
+        try{
+      	  $query = "SELECT orders.*, users.firstName, users.lastName
+          FROM orders
+          LEFT JOIN users
+          ON orders.userID=users.userID
+          WHERE users.email LIKE :email";
+
+      	  $stmt = $pdo->prepare($query);
+
+      	  $stmt->bindValue(':email', $_POST['email']);
+          $stmt->execute();
+
+          //Output Table
+          echo '
+          <section id="view-order">
+            <div class="container">
+          		<table>
+          			<tr>
+          				<th>ID</th>
+          				<th>Customer</th>
+          				<th>Order Overview</th>
+                  <th>Pickup</th>
+                  <th>Delivery</th>
+                  <th>Status</th>
+                  <th>More Details...</th>
+      			</tr>';
+          foreach($stmt as $order)
+          {
+            echo "
+              <tr>
+                <td>{$order['orderID']}</td>
+                <td>{$order['firstName']} {$order['lastName']}</td>
+                <td>
+                  <p>Desc:{$order['description']}</p>
+                  <p>Weight:{$order['totalWeight']}KG</p>
+                  <p>Type:{$order['deliveryPriority']}</p>
+                </td>
+                <td>
+                  <p>Time:{$order['pickUptime']}</p>
+                  <p>Address:{$order['pickUpAddress']}</p>
+                </td>
+                <td>
+                  <p>Recipient:{$order['recipientName']}</p>
+                  <p>Recipient Phone:{$order['recipientPhone']}</p>
+                  <p>Address:{$order['deliveryAddress']}</p>
+                </td>
+                <td>{$order['orderStatus']}</td>
+                <td><a href='view-order.php?orderID={$order['orderID']}'>View</a></td>
+              </tr>
+            ";
+          }
+          echo "
+              </table>
+            </div>
+          </section>";
+          $orders = $stmt->fetch();
+        } catch (PDOException $e){
+          echo $e->getMessage();
+        }
+
+
+    	}/*else if(isset($_POST['email']) && isset($_POST['']))
+    	{
+    	  $query = "SELECT *
+    	  FROM Orders ord, Users u
+    	  WHERE ord.userID == u.userID
+    	  AND u.email LIKE :email
+    	  AND ord.orderStatus == :status";
 
     	  $stmt = $pdo->prepare($query);
 
     	  $stmt->bindValue(':email', $_POST['email']);
-        $stmt->execute();
-
-        //Output Table
-        echo '
-        <section id="view-order">
-          <div class="container">
-        		<table>
-        			<tr>
-        				<th>ID</th>
-        				<th>Customer</th>
-        				<th>Description</th>
-                <th>Total Weight</th>
-                <th>Priority</th>
-                <th>Status</th>
-                <th>More Details...</th>
-    			</tr>';
-        foreach($stmt as $order)
-        {
-          echo "
-          <tr>
-            <td>{$order['orderID']}</td>
-            <td>{$order['firstName']} {$order['lastName']}</td>
-            <td>{$order['description']}</td>
-            <td>{$order['totalWeight']}</td>
-            <td>{$order['deliveryPriority']}</td>
-            <td>{$order['orderStatus']}</td>
-            <td><a href='view-order.php?orderID={$order['orderID']}'>View</a></td>
-          </div>
-        </section>
-          ";
-        }
-        $orders = $stmt->fetch();
-      } catch (PDOException $e){
-        echo $e->getMessage();
-      }
-
-
-  	}/*else if(isset($_POST['email']) && isset($_POST['']))
-  	{
-  	  $query = "SELECT *
-  	  FROM Orders ord, Users u
-  	  WHERE ord.userID == u.userID
-  	  AND u.email LIKE :email
-  	  AND ord.orderStatus == :status";
-
-  	  $stmt = $pdo->prepare($query);
-
-  	  $stmt->bindValue(':email', $_POST['email']);
-  	  $stmt->bindValue(':status', $_POST['status']);
-  	}*/
+    	  $stmt->bindValue(':status', $_POST['status']);
+    	}*/
+    }
   }
-}
 ?>
 
 <?php require 'includes/header.inc' ?>
