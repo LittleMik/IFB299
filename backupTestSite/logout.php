59ce1645f6c82dd3913531b7639f2bea01c6bf60
Michael Smallcombe <Michael Smallcombe>
2016-09-22 19:01:52 +1000

    Removed old test website

--- backupTestSite/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();   
-	unset($_SESSION['isUser']);
-    unset($_SESSION['firstname']);
-    unset($_SESSION['userID']);
-	header('Location: '.$_GET['location']);
-	exit();
-?>
