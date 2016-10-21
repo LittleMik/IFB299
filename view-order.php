3574b5a8d0994c6fee4040648eefcbe5b2e49fb5
Michael Smallcombe <Michael Smallcombe>
2016-10-21 18:07:23 +1000

    Added Output Functions

--- view-order.php
+++ view-order.php
@@ -37,74 +37,21 @@
 					require_once 'php/formValidation.php';
 					if(checkIntID($_GET['orderID']))
 					{
-						echo '
-							<h3>Order</h3>
-							<table class="table table-striped table-condensed table-responsive">
-							<thead>
-								<tr>
-									<th>ID</th>
-									<th>Customer</th>
-									<th>Description</th>
-									<th>Pickup</th>
-									<th>Delivery</th>
-									<th>Recipient</th>
-									<th>Status</th>
-									<th> </th>
-								</tr>
-							</thead>
-							<tbody>';
-
-						//Display Order
+						//Retrieve Order
 						require_once 'php/orders.php';
 						$order = new Order();
 						$order->getOrder($_GET['orderID']);
-						$order->displayOrder();
 
-						//Close Table Tags
-						echo "</tbody></table>";
-
-						//Output Packages Table
-						echo '
-							<h3>Packages</h3>
-							<table class="table table-striped table-condensed table-responsive">
-								<thead>
-									<tr>
-										<th>ID</th>
-										<th>Description</th>
-										<th>Weight</th>
-									</tr>
-								</thead>
-								<tbody>';
-
-						//Display Packages
-						require_once 'php/packages.php';
+						//Retrieve Order's Packages
 						$packages = $order->getPackages();
 
-						//Check if packages are present
-						if(empty($packages))
-						{
-							echo "<tr><td colspan='3'><h4>No packages can be found for this order...</h4></td></tr>";
-						}
-							else
-						{
-							foreach($packages as $package)
-							{
-								//Output Each Package as a Row
-								$package->displayPackage();
-							}
-						}
+						//Retrieve Payment Information
+						require_once 'php/paymentsDB.php';
+						$payment = getPayment($_GET['orderID']);
 
-						//Close Table Tags
-						echo '</tbody></table>';
-
-						//Verify User Permission to View Payments
-						require_once 'php/permissions.php';
-						if(checkPermission($_SESSION['role'], 'payments-view') === true)
-						{
-							require_once 'php/paymentsDB.php';
-							displayPayment(getPayment($_GET['orderID']));
-						}
-
+						//Output All Order Relevant Information
+						require_once 'php/output.php';
+						outputOrderAll($order, $packages, $payment);
 					}
 						else
 					{
